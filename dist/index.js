ac1e736151abd882684fec498c4d4b008e249ce1
Twaites <kory@twaites.com>
2025-02-10 00:58:47 -0800

    adding keep alive

--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
+const election_1 = __importDefault(require("./services/election"));
 dotenv_1.default.config();
 console.log(`Server ${process.env.FLY_MACHINE_ID} is starting...`);
+// Keep the process alive
+process.stdin.resume();
+// Handle shutdown gracefully
+process.on('SIGTERM', () => {
+    console.log('Received SIGTERM. Shutting down...');
+    election_1.default.stop();
+    process.exit(0);
+});
+process.on('SIGINT', () => {
+    console.log('Received SIGINT. Shutting down...');
+    election_1.default.stop();
+    process.exit(0);
+});
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,19 @@ import election from "./services/election";
 dotenv.config();
 
 console.log(`Server ${process.env.FLY_MACHINE_ID} is starting...`);
+
+// Keep the process alive
+process.stdin.resume();
+
+// Handle shutdown gracefully
+process.on('SIGTERM', () => {
+    console.log('Received SIGTERM. Shutting down...');
+    election.stop();
+    process.exit(0);
+});
+
+process.on('SIGINT', () => {
+    console.log('Received SIGINT. Shutting down...');
+    election.stop();
+    process.exit(0);
+});
