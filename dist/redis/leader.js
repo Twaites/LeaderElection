315ec3cbbce0457d9892359b8ae4fd63d7a47833
Twaites <kory@twaites.com>
2025-02-10 00:35:50 -0800

    updating redis build

--- dist/redis/leader.js
+++ dist/redis/leader.js
@@ -6,16 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getRedisLeader = exports.tryBecomeLeader = void 0;
 const index_1 = __importDefault(require("./index"));
 const config_1 = require("../config");
+const logger_1 = __importDefault(require("../utils/logger"));
 async function tryBecomeLeader() {
-    const success = await index_1.default.set(config_1.LEADER_KEY, process.env.FLY_MACHINE_ID, 'EX', config_1.LEADER_TTL, 'NX');
-    if (success) {
-        const ttl = await index_1.default.ttl(config_1.LEADER_KEY);
-        console.log(`Leader TTL set to: ${ttl} seconds`);
+    try {
+        const success = await index_1.default.set(config_1.LEADER_KEY, process.env.FLY_MACHINE_ID, 'EX', config_1.LEADER_TTL, 'NX');
+        return success !== null;
     }
-    return success !== null;
+    catch (error) {
+        (0, logger_1.default)(`Redis error in tryBecomeLeader: ${error}`);
+        return false;
+    }
 }
 exports.tryBecomeLeader = tryBecomeLeader;
 async function getRedisLeader() {
-    return await index_1.default.get(config_1.LEADER_KEY);
+    try {
+        return await index_1.default.get(config_1.LEADER_KEY);
+    }
+    catch (error) {
+        (0, logger_1.default)(`Redis error in getRedisLeader: ${error}`);
+        return null;
+    }
 }
 exports.getRedisLeader = getRedisLeader;
--- dist/services/election.js
+++ dist/services/election.js
@@ -3,26 +3,55 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.electLeader = void 0;
+exports.lastKnownLeader = exports.isCurrentLeader = exports.electLeader = void 0;
 const leader_1 = require("../redis/leader");
 const leader_2 = require("../db/leader");
 const logger_1 = __importDefault(require("../utils/logger"));
 const config_1 = require("../config");
-const LEADER_REFRESH_INTERVAL = 60 * 1000; // 1 minute
+const LEADER_REFRESH_INTERVAL = Math.floor(config_1.LEADER_TTL * 0.6) * 1000; // Refresh at 60% of TTL
+const FOLLOWER_CHECK_INTERVAL = Math.floor(config_1.LEADER_TTL * 0.8) * 1000; // Check less frequently as follower
+let isCurrentLeader = false;
+exports.isCurrentLeader = isCurrentLeader;
+let lastKnownLeader = null;
+exports.lastKnownLeader = lastKnownLeader;
+let lastCheckTime = 0;
 async function electLeader() {
-    const redisLeader = await (0, leader_1.getRedisLeader)();
-    if (!redisLeader) {
-        (0, logger_1.default)("No leader in Redis. Attempting to become leader...");
-        if (await (0, leader_1.tryBecomeLeader)()) {
-            (0, logger_1.default)(`${process.env.FLY_MACHINE_ID} is now the leader`);
-            await (0, leader_2.updateLeader)(process.env.FLY_MACHINE_ID);
+    const now = Date.now();
+    try {
+        if (isCurrentLeader) {
+            // Leader only needs to refresh its key periodically
+            if (now - lastCheckTime >= LEADER_REFRESH_INTERVAL) {
+                if (await (0, leader_1.tryBecomeLeader)()) {
+                    await (0, leader_2.updateLeader)(process.env.FLY_MACHINE_ID);
+                    lastCheckTime = now;
+                }
+                else {
+                    exports.isCurrentLeader = isCurrentLeader = false;
+                    exports.lastKnownLeader = lastKnownLeader = null;
+                    (0, logger_1.default)(`${process.env.FLY_MACHINE_ID} lost leadership`);
+                }
+            }
         }
         else {
-            (0, logger_1.default)("Failed to become leader - another instance may have won the election");
+            // Non-leaders check less frequently
+            if (now - lastCheckTime >= FOLLOWER_CHECK_INTERVAL) {
+                const redisLeader = await (0, leader_1.getRedisLeader)();
+                if (!redisLeader && await (0, leader_1.tryBecomeLeader)()) {
+                    exports.isCurrentLeader = isCurrentLeader = true;
+                    exports.lastKnownLeader = lastKnownLeader = process.env.FLY_MACHINE_ID || null;
+                    (0, logger_1.default)(`${process.env.FLY_MACHINE_ID} is now the leader`);
+                    await (0, leader_2.updateLeader)(process.env.FLY_MACHINE_ID);
+                }
+                else {
+                    exports.lastKnownLeader = lastKnownLeader = redisLeader;
+                }
+                lastCheckTime = now;
+            }
         }
     }
-    else {
-        (0, logger_1.default)(`Current Redis leader: ${redisLeader}`);
+    catch (error) {
+        (0, logger_1.default)(`Error in election process: ${error}`);
+        // Don't reset lastCheckTime on error to prevent rapid retries
     }
 }
 exports.electLeader = electLeader;
