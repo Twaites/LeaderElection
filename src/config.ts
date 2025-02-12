bf542a6b704e464c5ec1818cfd4ea70cefc6e238
Twaites <kory@twaites.com>
2025-02-11 21:31:18 -0800

    Increasing to 2 minute checks

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const LEADER_KEY = "current_leader";
-export const LEADER_TTL = 120; // 2 minutes in seconds
+export const LEADER_TTL = 240; // 4 minutes in seconds
 export const LEADER_REFRESH_RATIO = 0.6; // Refresh at 60% of TTL
 export const FOLLOWER_CHECK_RATIO = 0.8; // Check at 80% of TTL
 
